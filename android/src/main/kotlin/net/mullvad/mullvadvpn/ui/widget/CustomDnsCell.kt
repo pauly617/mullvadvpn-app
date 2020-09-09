d30d76959538cbb0987482ff9d0e5f380d6cf473
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-09-09 21:18:07 +0000

    Create `CustomDnsCell` widget

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/widget/CustomDnsCell.kt
@@ -0,0 +1,54 @@
+package net.mullvad.mullvadvpn.ui.widget
+
+import android.content.Context
+import android.text.method.DigitsKeyListener
+import android.util.AttributeSet
+import android.widget.TextView
+import java.net.InetAddress
+import org.apache.commons.validator.routines.InetAddressValidator
+
+class CustomDnsCell : InputCell {
+    var onSubmitDnsServer: ((InetAddress?) -> Unit)? = null
+
+    constructor(context: Context) : super(context, TextView(context)) {}
+
+    constructor(context: Context, attributes: AttributeSet) :
+        super(context, attributes, TextView(context)) {}
+
+    constructor(context: Context, attributes: AttributeSet, defaultStyleAttribute: Int) :
+        super(context, attributes, defaultStyleAttribute, TextView(context)) {}
+
+    constructor(
+        context: Context,
+        attributes: AttributeSet,
+        defaultStyleAttribute: Int,
+        defaultStyleResource: Int
+    ) : super(
+        context,
+        attributes,
+        defaultStyleAttribute,
+        defaultStyleResource,
+        TextView(context)
+    ) {}
+
+    init {
+        input.apply {
+            @Suppress("DEPRECATION")
+            keyListener = object : DigitsKeyListener() {
+                override fun getAcceptedChars() = "01234567890abcdefABCDEF.:".toCharArray()
+            }
+        }
+
+        isValidInput = { input -> isValid(input) }
+
+        onSubmitText = { input ->
+            if (isValid(input)) {
+                onSubmitDnsServer?.invoke(InetAddress.getByName(input))
+            } else {
+                onSubmitDnsServer?.invoke(null)
+            }
+        }
+    }
+
+    private fun isValid(input: String) = InetAddressValidator.getInstance().isValid(input)
+}
